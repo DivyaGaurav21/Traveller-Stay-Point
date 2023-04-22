652825636df721e9f9fc7665b1302fe2c40ccce9
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-04-22 17:15:40 +0530

    add all subcomponent and linked all in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
-
+import About from './Components/About/About';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Offer from './Components/Offers/Offer';
+import Popular from './Components/Popular/Popular';
+import Blog from './Components/Blog/Blog'
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <p>Divya</p>
+      <Navbar/>
+      <Home/>
+      <Popular/>
+      <Offer/>
+      <About/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
