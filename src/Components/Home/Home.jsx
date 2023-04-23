09bda7921378c0bf0aaddb6395f7aa9ba3958828
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-04-23 14:53:45 +0530

    add navBar and Home Componenent

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="secContainer container">
+        <div className="homeText">
+
+          <h1 className="title">
+            Plan Your Trip With HomeHaven.
+          </h1>
+
+          <p className="subTitle">
+            Travel to favorite city and stay in respectful environment!
+          </p>
+          <a href="#" className="btn signupBtn exploreBtn">
+              Explore Now
+          </a>
+        </div>
+
+        <div className="homeCard grid">
+
+          <div className="locationDiv">
+            <label htmlFor="location">Location</label>
+            <input type="text" placeholder='Dream Destination' />
+          </div>
+
+          <div className="priceDiv">
+            <label htmlFor="price">Price</label>
+            <input type="text" placeholder='₹5000-₹30000' />
+          </div>
+
+          <button className='btn signupBtn'>Search</button>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React , {useState} from 'react'
+import './navbar.css'
+import {FcHome} from 'react-icons/fc'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {TbGridDots} from 'react-icons/tb'
 
 const Navbar = () => {
+
+  // code to show navbar 
+const [active , setActive] = useState('navBar');
+
+const showNav = () => {
+  setActive('navBar activeNavbar')
+}
+
+const hideNav = () => {
+  setActive('navBar');
+}
+
   return (
-    <div>Navbar</div>
+     <section className='navBarSection'>
+        <div className="header">
+
+            <div className="logoDiv">
+                <a href="#" className="logo flex">
+                    <h1>
+                    <FcHome className='icon'/>
+                     HomeHaven
+                    </h1>
+                </a>
+            </div>
+
+            <div className={active}>
+
+                <ul className="navLists flex">
+
+                     <li className="navItem">
+                    <a href="#" className="navLink">Home</a>
+                    </li> 
+                    <li className="navItem">
+                    <a href="#" className="navLink">Products</a>
+                    </li> 
+                    <li className="navItem">
+                    <a href="#" className="navLink">Resources</a>
+                    </li> 
+                    <li className="navItem">
+                    <a href="#" className="navLink">Contact</a>
+                    </li> 
+                    <li className="navItem">
+                    <a href="#" className="navLink">Blog</a>
+                    </li> 
+
+                  <div className="headerBtns flex">
+                      <a href="#" className="btn loginBtn">Login</a>
+                      <a href="#" className="btn signupBtn">Sign Up</a>
+                  </div>
+
+                </ul>
+
+                <div className="closeNavbar" onClick={hideNav}>
+                    <AiFillCloseCircle className='icon'/>
+                </div>
+            </div>
+
+            <div className="toggleNavbar" onClick={showNav}>
+                 <TbGridDots className='icon'/>
+            </div>
+
+        </div>
+     </section>
   )
 }
 
